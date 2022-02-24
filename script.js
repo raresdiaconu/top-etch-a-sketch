bd2405fba1d2c013e6be94ba396698d400163b31
Rares Diaconu <98269458+raresdiaconu@users.noreply.github.com>
2022-02-25 01:50:44 +0200

    cleaned up code, added pencil, rainbow, eraser

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const sketchPad = document.getElementById("canvas");
-
 let pixelsPerSide = 20;
-const canvasSize = 700;
+const canvas = document.getElementById("canvas");
+const canvasSize = getComputedStyle(canvas).width.substring(0, 3);
+const rainbowColors = ["#ff0000", "#ffa500", "#ffff00", "#008000", "#0000ff", "#4b0082", "#ee82ee"];
+
 
 document.addEventListener("DOMContentLoaded", sketchResolution(pixelsPerSide));
 
@@ -11,9 +12,9 @@ function sketchResolution(pixelsPerSide) {
         pixel.classList.add('pixel');
         pixel.style.height = (canvasSize / pixelsPerSide) + "px";
         pixel.style.width = (canvasSize / pixelsPerSide) + "px";
-        sketchPad.appendChild(pixel);
+        pixel.addEventListener("mouseover", colorPixels);
+        canvas.appendChild(pixel);
     }
-    activateHover();
 }
 
 const slider = document.querySelector(".slider-resolution");
@@ -41,48 +42,51 @@ function clearBoard() {
 
 const colorPicker = document.querySelector(".color-picker");
 let currentColor = colorPicker.value;
-colorPicker.addEventListener("input", updateColor);
-function updateColor(e) {
-    currentColor = e.target.value;
-}
+colorPicker.addEventListener("change", (e) => currentColor = e.target.value);
 
-
-function activateHover() {
-    const pixels = document.querySelectorAll(".pixel");
-    pixels.forEach(pixel => pixel.addEventListener("mousedown", colorPixels));
-    function colorPixels() {
-        pixels.forEach(pixel => pixel.addEventListener("mousemove", colorPixels));
-        let i = Math.floor(Math.random() * rainbowColors.length);
-        this.style.backgroundColor = rainbowColors[i];
-        // this.style.backgroundColor = currentColor;
-        pixels.forEach(pixel => pixel.addEventListener("mouseup", removeListener));
-        function removeListener() {
-            pixels.forEach(pixel => pixel.removeEventListener("mousemove", colorPixels));
-        }
-    }
-}
-
-// const unicornBtn = document.querySelector(".unicorn");
-// const rainbowColors = ["#ff0000", "#ffa500", "#ffff00", "#008000", "#0000ff", "#4b0082", "#ee82ee"];
-// unicornBtn.addEventListener("click", activateUnicorn)
-
-
-const eraseBtn = document.querySelector(".eraser");
-
-eraseBtn.addEventListener("click", activateEraser);
-function activateEraser() {
-    const pixels = document.querySelectorAll(".pixel");
-    pixels.forEach(pixel => pixel.addEventListener("mousedown", clearPixel));
-    function clearPixel() {
-        pixels.forEach(pixel => pixel.addEventListener("mousemove", clearPixel));
-        this.style.backgroundColor = '#fff';
-        pixels.forEach(pixel => pixel.addEventListener("mouseup", removeListener));
-        function removeListener() {
-            pixels.forEach(pixel => pixel.removeEventListener("mousemove", clearPixel))
-        }
-    }
-}
 
 const pencilBtn = document.querySelector(".pencil");
-pencilBtn.addEventListener("click", activateHover);
+const randomBtn = document.querySelector(".rainbow");
+const eraserBtn = document.querySelector(".eraser");
+const lighterBtn = document.querySelector(".lighter");
 
+randomBtn.addEventListener("click", color);
+pencilBtn.addEventListener("click", picker);
+pencilBtn.addEventListener("click", color);
+eraserBtn.addEventListener("click", color);
+lighterBtn.addEventListener("click", color);
+
+function picker(e) {
+    currentColor = colorPicker.value;
+}
+
+let currentValue = ""
+function color(e) {
+    currentValue = e.target.classList.value;
+}
+
+
+function colorPixels(e) {
+    // console.log(currentValue)   
+    if (currentValue === "rainbow") {
+        let i = Math.floor(Math.random() * rainbowColors.length);
+        currentColor = rainbowColors[i];
+    }
+
+    if (currentValue === "eraser") {
+        currentColor = "#fff";
+    }
+
+    if (currentValue === "lighter") {
+        currentColor = "rgba(51, 180, 51, .1)";
+    }
+    
+    e.target.style.backgroundColor = currentColor;
+}
+
+
+
+// let R = Math.floor(Math.random() * 255);
+// let G = Math.floor(Math.random() * 255);
+// let B = Math.floor(Math.random() * 255);
+// currentColor = `rgb(${R}, ${G}, ${B})`;
